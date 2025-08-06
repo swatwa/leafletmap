2182940e54a3e90ad3b4e56aefaa3c180796d62e
Paradiso <Nathan.Paradiso@lumen.com>
2025-08-06 15:43:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+});
+
+const Imagery = L.tileLayer(
+  "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+  }
+);
+
+const map = L.map("map", {
+  center: [47.2529, -122.4443],
+  zoom: 9,
+  layers: [Imagery], // default layer
+});
+
+const baseMaps = {
+  OpenStreetMap: osm,
+  Imagery: Imagery,
+};
+
+L.control.layers(baseMaps).addTo(map);
+
+L.marker([47.2529, -122.4443]).addTo(map).bindPopup("Test").openPopup();
