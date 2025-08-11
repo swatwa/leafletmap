80b3d1b343d5d7134cf691f8b3853e9a00582084
Paradiso <Nathan.Paradiso@lumen.com>
2025-08-11 16:05:07 -0700

    styingling

--- script.js
+++ script.js
@@ -33,8 +33,7 @@ const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 const Imagery = L.tileLayer(
   "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   {
-    attribution:
-      "Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    attribution: "Tiles © Esri",
   }
 );
 
